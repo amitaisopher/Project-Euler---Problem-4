9b4dd7f7f623ba9620d1e66a44496eb7448fe207
Amitai Sopher <amitaisopher@gmail.com>
2016-02-23 22:02:28 +0200

    complete new code

--- src/PalindromeNumber.java
+++ src/PalindromeNumber.java
@@ -3,53 +3,30 @@
  */
 public class PalindromeNumber {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
-        for (int i = 999; i > 0 ; i--) {
-            PalindromeNumber num = null;
-            for (int j = i; j > 0; j--) {
-                num = new PalindromeNumber(i, j);
-                num.checkIfPalindrome();
-                if (num.isPalindrome)
+        for (int i = 999; i > 0; i--) {
+            String stringNum = (Integer.toString(i));
+            char[] firstHalf = stringNum.toCharArray();
+            char[] secondHalf = reverseCharacters(firstHalf);
+            String secondHalfString = String.copyValueOf(secondHalf);
+            String palindromicString = stringNum + secondHalfString;
+            int palindromicNum = Integer.parseInt(palindromicString);
+            System.out.println("The current palindromic number is: " + palindromicNum);
+            for (int j = 999; j > 0; j--) {
+                if (palindromicNum % j == 0 && palindromicNum/j < 1000) {
+                    System.out.println(" The largest palindrome number found is: " + palindromicNum + " and its factros are: " + j + " , " + palindromicNum / j);
                     return;
-            }
-            if (num.isPalindrome)
-                return;
-        }
-
-    }
-
-    private int value = 0;
-    private int firstFactor = 0;
-    private int secondFactor = 0;
-    private boolean isPalindrome = false;
-
-    public PalindromeNumber (int firsNum, int secondNum) {
-        this.value = firsNum * secondNum;
-        this.firstFactor = firsNum;
-        this.secondFactor = secondNum;
-        this.isPalindrome = false;
-    }
-
-    private void checkIfPalindrome () {
-        int length = String.valueOf(this.value).length();
-        String valueString = String.valueOf(this.value);
-        for (int i = 0; i < (length / 2) ; i++) {
-            char a_char = valueString.charAt(i);
-            char b_char = valueString.charAt(length-(i+1));
-            if (a_char != b_char) {
-                return;
-            } else {
-                if (i == (length/2 -1)) {
-                    this.isPalindrome = true;
-                    System.out.println("This is a Palindrome and its value is: " + this.value + " and the two factors are: " + this.firstFactor + " , " + this.secondFactor);
                 }
             }
         }
     }
 
-    @Override
-    public String toString () {
-        return "The value of Palindrome is " + this.value + " and its two factors are " + this.firstFactor + " , " + this.secondFactor;
+    private static char[] reverseCharacters(char[] origArray) {
+        char[] reversedCharacterArray = new char[3];
+        for (int i = origArray.length - 1; i >= 0; i--) {
+            reversedCharacterArray[origArray.length - (i + 1)] = origArray[i];
+        }
+        return reversedCharacterArray;
     }
 }
